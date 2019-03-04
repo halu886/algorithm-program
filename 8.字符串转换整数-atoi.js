4146f08fb9218c2280d8e97c336199f4b69112f8
halu886 <halu886@qq.com>
2019-03-04 10:21:24 +0800

    字符串转换整数

--- 8.字符串转换整数-atoi.js
+++ 8.字符串转换整数-atoi.js
@@ -70,7 +70,16 @@
  * @param {string} str
  * @return {number}
  */
-var myAtoi = function(str) {
-    
+var myAtoi = function (str) {
+    const regex = /\s*([-+])?(\d*)/
+    const match = str.match(regex);
+    let number = Number(match[1] === '-' ? -1 : 1) * Number(match[2] || 0);
+    if (number > (2 ** 31 - 1)) {
+        number = 2 ** 31 - 1;
+    }
+    if (number < -(2 ** 31)) {
+        number = -(2 ** 31);
+    }
+    return number;
 };
 
