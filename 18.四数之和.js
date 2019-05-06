4f54a535cfe1b7ca9f759e6ae888569dda873dcb
halu886 <halu886@qq.com>
2019-05-06 13:38:05 +0800

    四数之和

---
+++ 18.四数之和.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=18 lang=javascript
+ *
+ * [18] 四数之和
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+    const arrs = []
+    function recursionCount(index) {
+
+    }
+
+    for (let i = 0; i < nums.length - 4; i++) {
+
+    }
+};
+
