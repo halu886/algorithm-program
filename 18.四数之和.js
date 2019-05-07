14098a25bdaff785ca0d9e40068b871be76b86cc
halu886 <halu886@qq.com>
2019-05-07 10:24:15 +0800

    四数之和

--- 18.四数之和.js
+++ 18.四数之和.js
@@ -10,11 +10,26 @@
  */
 var fourSum = function (nums, target) {
     const arrs = []
-    function recursionCount(index) {
+    nums = nums.sort((x, y) => x < y);
+    let i = 0, j = nums.length - 1;
+    while (j - i < 2) {
+        let x = i + 1, z = j - 1;
+        while (x < z) {
+            const arr = []
+            const sum = nums[x] + nums[y] + nums[i] + nums[j];
+            if (sum === target) {
+                arr.push(nums[x], nums[y], nums[i], nums[j]);
+                break;
+            }
 
-    }
-
-    for (let i = 0; i < nums.length - 4; i++) {
+            if (sum < target) {
+                while (nums[x] === nums[x + 1] && x < z)++x;
+                ++x
+            } else if (sum > target) {
+                while (nums[z] === nums[z - 1] && x < z)--z;
+                --z;
+            }
+        }
 
     }
 };
