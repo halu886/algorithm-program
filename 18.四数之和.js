9a51e10c335612b0d7eaf8c7db66e351ab88ecb2
halu886 <halu886@qq.com>
2019-05-08 09:23:17 +0800

    四数之和

--- 18.四数之和.js
+++ 18.四数之和.js
@@ -10,27 +10,39 @@
  */
 var fourSum = function (nums, target) {
     const arrs = []
-    nums = nums.sort((x, y) => x < y);
+    nums = nums.sort((x, y) => x > y);
     let i = 0, j = nums.length - 1;
-    while (j - i < 2) {
+    let guard = false;
+    console.log(nums)
+    while (j - i >= 2) {
         let x = i + 1, z = j - 1;
         while (x < z) {
-            const arr = []
-            const sum = nums[x] + nums[y] + nums[i] + nums[j];
+            console.log(nums[x], nums[z], nums[i], nums[j])
+            const sum = nums[x] + nums[z] + nums[i] + nums[j];
             if (sum === target) {
-                arr.push(nums[x], nums[y], nums[i], nums[j]);
+                arrs.push([nums[x], nums[z], nums[i], nums[j]]);
                 break;
             }
 
             if (sum < target) {
                 while (nums[x] === nums[x + 1] && x < z)++x;
-                ++x
+                ++x;
             } else if (sum > target) {
                 while (nums[z] === nums[z - 1] && x < z)--z;
                 --z;
             }
         }
-
+        if (guard) {
+            guard = false;
+            while (nums[i] === nums[i + 1] && (j - i >= 2))++i;
+            ++i;
+        } else {
+            guard = true;
+            while (nums[j] === nums[j - 1] && (j - i >= 2))--j;
+            --j;
+        }
     }
+    console.log(arrs);
+    return arrs;
 };
 
