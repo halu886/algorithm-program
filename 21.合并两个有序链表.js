c7ae2bf486fd8d10e1f407cd49de3d8530d03bf3
halu886 <halu886@qq.com>
2019-05-23 09:58:03 +0800

    合并两个有序链表

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -16,14 +16,12 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
-    console.log(l1, l2);
     let pHead = null;
     let pHead1 = l1;
     let pHead2 = l2;
     let p1 = l1;
     let p2 = l2;
     let p = null;
-
     if (!l1) {
         return l2;
     } else if (!l2) {
@@ -32,18 +30,15 @@ var mergeTwoLists = function (l1, l2) {
 
     if (l1.val < l2.val) {
         pHead = p1;
-        p = p1.next;
+        p = p1;
         p1 = p1.next;
     } else {
         pHead = p2;
-        p = p2.next;
+        p = p2;
         p2 = p2.next;
     }
 
-    console.log(p1.val, pHead1.val, p2.val, pHead2.val, p1 === pHead1, p2 === pHead2);
-    console.log("start");
-    while (p1 === pHead1 || p2 === pHead2) {
-        console.log(p1.val, pHead1.val, p2.val, pHead2.val, p1 === pHead1, p2 === pHead2);
+    while (p1 && p2) {
         if (p1.val < p2.val) {
             p.next = p1;
             p = p.next;
@@ -51,36 +46,13 @@ var mergeTwoLists = function (l1, l2) {
         } else {
             p.next = p2;
             p = p.next;
-            p2 = p2.next;
-        }
-    }
-    console.log("end");
-    console.log(p1.val, pHead1.val, p2.val, pHead2.val, p1 === pHead1, p2 === pHead2);
-    console.log("start");
-    while (p1 !== null && p1 !== pHead1 && p1 !== null && p2 !== pHead2) {
-        console.log(p1.val, pHead1.val, p2.val, pHead2.val, p1 === pHead1, p2 === pHead2);
-        if (p1.val < p2.val) {
-            p.next = p1;
-            p = p.next;
-            p1 = p1.next;
-        } else {
-            p.next = p2;
-            p = p.next;
-            console.log(p2)
             p2 = p2.next;
         }
     }
 
     if (p1) {
-        // while (p1.next !== pHead1) {
-        //     p1 = p1.next;
-        // }
-        // p1.next = pHead
         p.next = p1;
     } else if (p2) {
-        // while (p2.next !== pHead2) {
-        //     p2 = p2.next;
-        // }
         p.next = p2;
     }
 
