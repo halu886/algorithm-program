f5e49495b5b10e2d9b1b939197ac6eac22040664
halu886 <halu886@qq.com>
2019-05-27 09:58:45 +0800

    括号生成

--- 22.括号生成.cpp
+++ 22.括号生成.cpp
@@ -3,10 +3,22 @@
  *
  * [22] 括号生成
  */
-class Solution {
+class Solution
+{
 public:
-    vector<string> generateParenthesis(int n) {
-        
+    vector<string> generateParenthesis(int n)
+    {
+        string tempLeft = "";
+        string tempRight = "";
+        for (int i = 0; i < n; i++)
+        {
+            tempLeft += '(';
+            tempRight += ')';
+        }
+        string temp = tempLeft + tempRight;
+
+        {
+                }
+        while ()
     }
 };
-
