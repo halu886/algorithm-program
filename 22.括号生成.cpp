a8d05d1de137db3e4da990d226679eda8168f34e
halu886 <halu886@qq.com>
2019-05-29 10:51:26 +0800

    括号生成

--- 22.括号生成.cpp
+++ 22.括号生成.cpp
@@ -8,17 +8,52 @@ class Solution
 public:
     vector<string> generateParenthesis(int n)
     {
-        string tempLeft = "";
-        string tempRight = "";
-        for (int i = 0; i < n; i++)
-        {
-            tempLeft += '(';
-            tempRight += ')';
-        }
-        string temp = tempLeft + tempRight;
+        vector<string> resultV = new verctor();
 
+        recursion(&resultV, "", n);
+
+        return resultV;
+    }
+
+public:
+    void recursion(Vector *pResult, String str, int n)
+    {
+        if (str.size() == n)
         {
+            if (isValid(str))
+            {
+                *pResult.push(str);
+            }
+            else
+            {
+                return;
+            }
         }
-        while ()
+
+        if (isValid(str))
+        {
+            recursion(pResult, str + "(", n);
+            recursion(pResult, str + ")", n);
+        }
+    }
+
+public:
+    Boolean isValid(string str)
+    {
+        int balance = 0;
+
+        for (int i = 0; i < str.length, i++)
+        {
+            char c = str[i];
+            if (c == "(")
+            {
+                balance++;
+            }
+            else if (c == ")")
+            {
+                balance--;
+            }
+        }
+        return balance >= 0
     }
 };
