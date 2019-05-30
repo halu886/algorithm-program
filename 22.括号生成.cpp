0d151b6c02ebe594f34e031d3583295acffb4ddf
halu886 <halu886@qq.com>
2019-05-30 09:49:57 +0800

    括号生成

--- 22.括号生成.cpp
+++ 22.括号生成.cpp
@@ -5,55 +5,60 @@
  */
 class Solution
 {
-public:
-    vector<string> generateParenthesis(int n)
-    {
-        vector<string> resultV = new verctor();
-
-        recursion(&resultV, "", n);
-
-        return resultV;
-    }
 
 public:
-    void recursion(Vector *pResult, String str, int n)
-    {
-        if (str.size() == n)
-        {
-            if (isValid(str))
-            {
-                *pResult.push(str);
-            }
-            else
-            {
-                return;
-            }
-        }
-
-        if (isValid(str))
-        {
-            recursion(pResult, str + "(", n);
-            recursion(pResult, str + ")", n);
-        }
-    }
-
-public:
-    Boolean isValid(string str)
-    {
-        int balance = 0;
-
-        for (int i = 0; i < str.length, i++)
-        {
-            char c = str[i];
-            if (c == "(")
-            {
-                balance++;
-            }
-            else if (c == ")")
-            {
-                balance--;
-            }
-        }
-        return balance >= 0
-    }
+    bool isValid(string str);
+    void recursion(vector<string> *pResult, string str, int n);
+    vector<string> generateParenthesis(int n);
 };
+
+vector<string> Solution::generateParenthesis(int n)
+{
+
+    vector<string> resultV();
+
+    recursion(&resultV, "", n);
+
+    return resultV;
+}
+
+void Solution::recursion(vector<string> *pResult, string str, int n)
+{
+    if (str.size() == n)
+    {
+        if (this.isValid(str))
+        {
+            *pResult.push(str);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    if (isValid(str))
+    {
+        recursion(pResult, str + "(", n);
+        recursion(pResult, str + ")", n);
+    }
+}
+
+bool Solution::isValid(string str)
+{
+    int balance = 0;
+
+    for (int i = 0; i < str.length, i++)
+    {
+        char c = str[i];
+        if (c == "(")
+        {
+            balance++;
+        }
+        else if (c == ")")
+        {
+            balance--;
+        }
+    }
+    // return balance >= 0
+    return true;
+}
