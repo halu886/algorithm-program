866c8db62a5b8f4d1a4955ae79feb756dd2acb88
halu886 <halu886@qq.com>
2019-07-03 09:59:26 +0800

    k-个一组翻转链表

---
+++ 25.k-个一组翻转链表.cpp
@@ -0,0 +1,49 @@
+    
+/*
+ * @lc app=leetcode.cn id=25 lang=cpp
+ *
+ * [25] k个一组翻转链表
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        ListNode *temp = head;
+        ListNode *mid = NULL;
+        double midIndex = (double)k / 2.0;
+        for (int i = 0; i < k; i++)
+        {
+            if (temp = NULL)
+            {
+                return head;
+            }
+            if (i <= midIndex && i + 1 > midIndex)
+            {
+                mid = temp;
+            }
+            temp = temp->next;
+        }
+
+        ListNode *first = head;
+        ListNode *second = head->next;
+        // k内所有节点指针往上翻转
+        for (int i = 0; i < k - 2; i++)
+        {
+            ListNode *third = second->next;
+            second->next = first;
+            first = second;
+            second = third;
+        }
+        head->next = reverseKGroup(second->next, k);
+        return second;
+    }
+};
