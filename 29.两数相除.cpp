cb04c56829d23550eeebd3a04ab2e36f61d9c0b9
halu886 <halu886@qq.com>
2019-07-23 09:33:46 +0800

    两数相除

--- 29.两数相除.cpp
+++ 29.两数相除.cpp
@@ -6,7 +6,53 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        
+        bool is_equel;
+        int dividend_symbol;
+        int divisor_symbol;
+        if(dividend<0){
+            dividend_symbol = -1;
+        }else{
+            dividend_symbol = 1;
+        }
+
+        if(divisor<0){
+            divisor_symbol = -1;
+        }else{
+            divisor_symbol = 1;
+        }
+
+        if(dividend_symbol==divisor_symbol){
+            is_equel = true;
+        }else{
+            is_equel = false;
+        }
+
+        int count = 0;
+        int add_count = 1;
+        int divisor_add = divisor;
+        int max = 2;
+        if(is_equel){
+            while(dividend-divisor>=dividend_symbol||dividend==divisor){
+                if(dividend-divisor_add>=dividend_symbol||dividend==divisor_add){
+                    dividend -= divisor_add;
+                    count += add_count;
+                }else{
+                    divisor_add -= divisor;
+                    --add_count;
+                }
+            }
+        }else{
+            while(dividend+divisor>=dividend_symbol||dividend==divisor){
+                if(dividend-divisor_add>=dividend_symbol||dividend==divisor_add){
+                    dividend -= divisor_add;
+                    count += add_count;
+                }else{
+                    divisor_add -= divisor;
+                    --add_count;
+                }
+            }
+        }
+        return count;
     }
 };
 
