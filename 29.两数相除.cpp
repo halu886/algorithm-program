6575400acd9a85d4cdafd26e029ded74a9fc5070
halu886 <halu886@qq.com>
2019-07-26 09:34:26 +0800

    两数相除

--- 29.两数相除.cpp
+++ 29.两数相除.cpp
@@ -3,27 +3,39 @@
  *
  * [29] 两数相除
  */
-class Solution {
+class Solution
+{
 public:
-    int divide(int dividend, int divisor) {
+    int divide(int dividend, int divisor)
+    {
         bool is_equel;
         int dividend_symbol;
         int divisor_symbol;
-        if(dividend<0){
+        cout << dividend << "\t" << divisor << "\t" << dividend_symbol << endl;
+        if (dividend < 0)
+        {
             dividend_symbol = -1;
-        }else{
+        }
+        else
+        {
             dividend_symbol = 1;
         }
 
-        if(divisor<0){
+        if (divisor < 0)
+        {
             divisor_symbol = -1;
-        }else{
+        }
+        else
+        {
             divisor_symbol = 1;
         }
 
-        if(dividend_symbol==divisor_symbol){
+        if (dividend_symbol == divisor_symbol)
+        {
             is_equel = true;
-        }else{
+        }
+        else
+        {
             is_equel = false;
         }
 
@@ -31,37 +43,63 @@ public:
         int add_count = 1;
         int divisor_add = divisor;
         int max = 2;
-        // cout << dividend_symbol << "\t" << divisor_symbol << "\t" << is_equel << endl;
-        if(is_equel){
-            while(dividend-divisor>=dividend_symbol||dividend==divisor){
-                if(dividend-divisor_add>=dividend_symbol||dividend==divisor_add){
+        cout << dividend_symbol << "\t" << divisor_symbol << "\t" << is_equel << endl;
+        if (is_equel)
+        {
+            cout << (dividend - divisor) << endl;
+            while (dividend - divisor >= dividend_symbol || dividend == divisor)
+            {
+                if (dividend == 0)
+                {
+                    break;
+                }
+                cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
+                if (dividend - divisor_add >= dividend_symbol || dividend == divisor_add)
+                {
                     dividend -= divisor_add;
                     count += add_count;
-                }else{
+                }
+                else
+                {
                     divisor_add -= divisor;
                     --add_count;
                 }
             }
-        }else{
-            if(dividend>0){
-                while(dividend+divisor>=dividend_symbol||dividend+divisor==0){
-                    cout << dividend << "\t"<<divisor<<"\t"<<divisor_add<<"\t"<<dividend_symbol<<"\t"<<count<<endl;
-                    if(dividend-divisor_add>=dividend_symbol||dividend+divisor_add==0){
-                        dividend -= divisor_add;
+        }
+        else
+        {
+            cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
+            if (dividend > 0)
+            {
+                while (dividend + divisor >= dividend_symbol || dividend + divisor == 0)
+                {
+                    cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
+                    if (dividend - divisor_add >= dividend_symbol || dividend + divisor_add == 0)
+                    {
+                        dividend += divisor_add;
                         count -= add_count;
-                    }else{
+                    }
+                    else
+                    {
                         divisor_add -= divisor;
                         --add_count;
                     }
                 }
-                cout << dividend << "\t"<<divisor<<"\t"<<divisor_add<<"\t"<<dividend_symbol<<"\t"<<count<<endl;
-            }else{
-                while(dividend+divisor<=dividend_symbol||dividend+divisor==0){
-                    // cout << dividend << "\t"<<divisor<<"\t"<<divisor_add<<"\t"<<dividend_symbol<<"\t"<<count<<endl;
-                    if(dividend+divisor_add<=dividend_symbol||dividend+divisor_add==0){
+                // cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
+            }
+            else
+            {
+                cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
+                while (dividend + divisor <= dividend_symbol || dividend + divisor == 0)
+                {
+                    cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
+                    if (dividend + divisor_add <= dividend_symbol || dividend + divisor_add == 0)
+                    {
                         dividend += divisor_add;
                         count -= add_count;
-                    }else{
+                    }
+                    else
+                    {
                         divisor_add -= divisor;
                         --add_count;
                     }
@@ -72,4 +110,3 @@ public:
         return count;
     }
 };
-
