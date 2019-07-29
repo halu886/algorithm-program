29c85f4439204b926afcb774606f0cc5a972a7c4
halu886 <halu886@qq.com>
2019-07-29 09:51:25 +0800

    两数相除

--- 29.两数相除.cpp
+++ 29.两数相除.cpp
@@ -8,105 +8,49 @@ class Solution
 public:
     int divide(int dividend, int divisor)
     {
-        bool is_equel;
-        int dividend_symbol;
-        int divisor_symbol;
-        cout << dividend << "\t" << divisor << "\t" << dividend_symbol << endl;
-        if (dividend < 0)
+        // 这题实在写不出了，直接抄了@liushang
+        if (dividend == 0)
+            return 0;
+
+        //  转换为long long型计算，防止 INT32_MIN / -1 溢出
+        long long lldividend = dividend, lldivisor = divisor;
+        long long cur_bit = 1, result = 0;
+
+        //  转换为整数计算，最后判断符号
+        lldividend = abs(lldividend);
+        lldivisor = abs(lldivisor);
+
+        //  先将被除数放大 cur_bit 倍
+        //  但是要保证被除数 <= 除数
+        while (lldividend >= lldivisor << 1)
         {
-            dividend_symbol = -1;
-        }
-        else
-        {
-            dividend_symbol = 1;
+            lldivisor <<= 1;
+            cur_bit <<= 1;
         }
 
-        if (divisor < 0)
+        //  除数依次减去被除数的 cur_bit 倍
+        //  右移 cur_bit 循环执行减法
+        //  直到 除数为 0 或者 cur_bit == 0
+        while (cur_bit > 0 && lldividend > 0)
         {
-            divisor_symbol = -1;
-        }
-        else
-        {
-            divisor_symbol = 1;
+            if (lldividend >= lldivisor)
+            {
+                lldividend -= lldivisor;
+                result += cur_bit;
+            }
+            cur_bit >>= 1;
+            lldivisor >>= 1;
         }
 
-        if (dividend_symbol == divisor_symbol)
-        {
-            is_equel = true;
-        }
-        else
-        {
-            is_equel = false;
-        }
+        //  判断 result 符号
+        result = (dividend > 0 && divisor < 0 ||
+                  dividend < 0 && divisor > 0)
+                     ? -result
+                     : result;
 
-        int count = 0;
-        int add_count = 1;
-        int divisor_add = divisor;
-        int max = 2;
-        cout << dividend_symbol << "\t" << divisor_symbol << "\t" << is_equel << endl;
-        if (is_equel)
-        {
-            cout << (dividend - divisor) << endl;
-            while (dividend - divisor >= dividend_symbol || dividend == divisor)
-            {
-                if (dividend == 0)
-                {
-                    break;
-                }
-                cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
-                if (dividend - divisor_add >= dividend_symbol || dividend == divisor_add)
-                {
-                    dividend -= divisor_add;
-                    count += add_count;
-                }
-                else
-                {
-                    divisor_add -= divisor;
-                    --add_count;
-                }
-            }
-        }
-        else
-        {
-            cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
-            if (dividend > 0)
-            {
-                while (dividend + divisor >= dividend_symbol || dividend + divisor == 0)
-                {
-                    cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
-                    if (dividend - divisor_add >= dividend_symbol || dividend + divisor_add == 0)
-                    {
-                        dividend += divisor_add;
-                        count -= add_count;
-                    }
-                    else
-                    {
-                        divisor_add -= divisor;
-                        --add_count;
-                    }
-                }
-                // cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
-            }
-            else
-            {
-                cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
-                while (dividend + divisor <= dividend_symbol || dividend + divisor == 0)
-                {
-                    cout << dividend << "\t" << divisor << "\t" << divisor_add << "\t" << dividend_symbol << "\t" << count << endl;
-                    if (dividend + divisor_add <= dividend_symbol || dividend + divisor_add == 0)
-                    {
-                        dividend += divisor_add;
-                        count -= add_count;
-                    }
-                    else
-                    {
-                        divisor_add -= divisor;
-                        --add_count;
-                    }
-                }
-                // cout << dividend << "\t"<<divisor<<"\t"<<divisor_add<<"\t"<<dividend_symbol<<"\t"<<count<<endl;
-            }
-        }
-        return count;
+        //  判断是否会溢出，并返回结果
+        if (result > INT32_MAX)
+            return INT32_MAX;
+        return (int)result;
     }
 };
