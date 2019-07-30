be60f6315699ff369f1b3619b95d4b6ee09e1a80
halu886 <halu886@qq.com>
2019-07-30 09:48:26 +0800

    串联所有单词的子串

---
+++ 30.串联所有单词的子串.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=30 lang=cpp
+ *
+ * [30] 串联所有单词的子串
+ */
+class Solution
+{
+public:
+    vector<int> findSubstring(string s, vector<string> &words)
+    {
+
+        for (int i = 0; i < vector.size; i++)
+        {
+        }
+    }
+};
