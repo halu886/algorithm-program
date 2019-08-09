dc33a5160f45baec105e9c05425f0d69852b061e
halu886 <halu886@qq.com>
2019-08-09 09:36:40 +0800

    下一个排列

--- 31.下一个排列.cpp
+++ 31.下一个排列.cpp
@@ -10,24 +10,43 @@ public:
     {
         int temp = 0;
         bool isArrange = false;
-        for (int i = nums.size() - 1; i > 0; i++)
+        for (int i = nums.size() - 1; i > 0 && !isArrange; i--)
         {
             if (nums[i] > nums[i - 1])
             {
-                isArrange = true;
-                temp = nums[i];
-                nums[i] = nums[i - 1];
-                nums[i - 1] = temp;
+                for (int j = i + 1; i < nums.size() && !isArrange; i++)
+                {
+                    if (nums[i - 1] > nums[j])
+                    {
+                        isArrange = true;
+                        temp = nums[i - 1];
+                        nums[i - 1] = nums[j - 1];
+                        nums[j - 1] = temp;
+                    }
+                }
+                if (!isArrange)
+                {
+                    isArrange = true;
+                    temp = nums[i - 1];
+                    nums[i - 1] = nums[nums.size() - 1];
+                    nums[nums.size() - 1] = temp;
+                }
             }
         }
 
-        if (isArrange)
+        if (!isArrange)
         {
             for (int i = 0; i < nums.size(); i++)
             {
                 for (int j = i; j < nums.size(); j++)
                 {
-                                }
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
             }
         }
     }
