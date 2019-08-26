9e7d2183eb41b10f1af4791b7aa9d49a3cfb63dd
halu886 <halu886@qq.com>
2019-08-26 10:02:51 +0800

    搜索旋转排序数组

--- 32.最长有效括号.cpp
+++ 32.最长有效括号.cpp
@@ -3,44 +3,90 @@
  *
  * [32] 最长有效括号
  */
+// class Solution
+// {
+// public:
+//     int longestValidParentheses(string s)
+//     {
+//         int leftSum = 0;
+//         int maxLength = 0;
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             char ch = s[i];
+//             if (ch == '(')
+//             {
+//                 ++leftSum;
+//                 ++maxLength;
+//             }
+//             else
+//             {
+//                 if (!leftSum)
+//                 {
+//                     string substr = s.substr(1);
+//                     if (substr.size() < maxLength)
+//                     {
+//                         return maxLength;
+//                     }
+//                     int anotherLength = longestValidParentheses(s.substr(1));
+//                     return anotherLength > maxLength ? anotherLength : maxLength;
+//                 }
+//                 --leftSum;
+//                 ++maxLength;
+//             }
+//         }
+
+//         if (leftSum)
+//         {
+//             int rightSub = longestValidParentheses(s.substr(1));
+//             int leftSub = longestValidParentheses(s.substr(0, s.size() - 1));
+//             return rightSub > leftSub ? rightSub : leftSub;
+//         }
+//         return maxLength;
+//     }
+// };
+
 class Solution
 {
 public:
     int longestValidParentheses(string s)
     {
-        int leftSum = 0;
         int maxLength = 0;
         for (int i = 0; i < s.size(); i++)
         {
-            char ch = s[i];
-            if (ch == '(')
+            for (int j = i + 2; j <= s.size(); j = j + 2)
             {
-                ++leftSum;
-                ++maxLength;
+                int length = j - i;
+                if (isValid(s.substr(i, length)) && maxLength < length)
+                {
+                    maxLength = length;
+                }
+            }
+        }
+        return maxLength;
+    };
+    bool isValid(string substr)
+    {
+        stack<char> strStack;
+        for (int i = 0; i < substr.size(); i++)
+        {
+            if (substr[i] == '(')
+            {
+                strStack.push('(');
+            }
+            else if (substr[i] == ')' && strStack.size() > 0)
+            {
+                strStack.pop();
             }
             else
             {
-                if (!leftSum)
-                {
-                    string substr = s.substr(1);
-                    if (substr.size() < maxLength)
-                    {
-                        return maxLength;
-                    }
-                    int anotherLength = longestValidParentheses(s.substr(1));
-                    return anotherLength > maxLength ? anotherLength : maxLength;
-                }
-                --leftSum;
-                ++maxLength;
+                return false;
             }
         }
 
-        if (leftSum)
+        if (strStack.size())
         {
-            int rightSub = longestValidParentheses(s.substr(1));
-            int leftSub = longestValidParentheses(s.substr(0, s.size() - 1));
-            return rightSub > leftSub ? rightSub : leftSub;
+            return false;
         }
-        return maxLength;
-    }
+        return true;
+    };
 };
---
+++ 33.搜索旋转排序数组.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=cpp
+ *
+ * [33] 搜索旋转排序数组
+ */
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int reversal = 0;
+        int begin = 0;
+        int end = nums[nums.size() - 1];
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                if (nums[i] < target || nums[i + 1] > target)
+                {
+                    return -1;
+                }
+                else if (nums[nums.size() - 1] > target)
+                {
+                    begin = i + 1;
+                }
+                else
+                {
+                    end = i;
+                }
+                reversal = i + 1;
+                break;
+            }
+        }
+    }
+};
