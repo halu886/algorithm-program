bfccac83104c88aa2780d40431c8f785d1c83953
halu886 <halu886@qq.com>
2019-09-06 10:05:03 +0800

    36

---
+++ 36.有效的数独.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=36 lang=cpp
+ *
+ * [36] 有效的数独
+ */
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            hashtable<char, int> x;
+            hashtable<char, int> y;
+            for (int j = 0; j < 9; j++)
+            {
+                if (x.has(board[i][j]))
+                {
+                    return false
+                }
+                if (y.has(board[j][i]))
+                {
+                    return false
+                }
+                x.push(board[i][j]),1)
+                y.push(board[j][i]),1)
+            }
+        }
+
+        for (int i = 0; i < 9; i += 3)
+        {
+
+            for (int i = 0; i < 9; i += 3)
+            {
+            }
+        }
+    }
+};
