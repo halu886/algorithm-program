981779319700c6b2197909f14888cbcddd849186
halu886 <halu886@qq.com>
2019-09-11 09:51:26 +0800

    36

--- 36.有效的数独.cpp
+++ 36.有效的数独.cpp
@@ -10,38 +10,39 @@ public:
     {
         for (int i = 0; i < 9; i++)
         {
-            hashtable<char, int> x;
-            hashtable<char, int> y;
+            unordered_map<char, int> x;
+            unordered_map<char, int> y;
             for (int j = 0; j < 9; j++)
             {
-                if (x.has(board[i][j]))
+                // cout << i << "\t" << j << "\t" << board[i][j] << "\t" << (x.find(board[i][j]) != x.end()) << endl;
+                // cout << i << "\t" << j << "\t" << board[j][i] << "\t" << (y.find(board[j][i]) != y.end()) << endl;
+                if (x.find(board[i][j]) != x.end() && board[i][j] != '.')
                 {
-                    return false
+                    return false;
                 }
-                if (y.has(board[j][i]))
+                if (y.find(board[j][i]) != y.end() && board[j][i] != '.')
                 {
-                    return false
+                    return false;
                 }
-                x.push(board[i][j]),1)
-                y.push(board[j][i]),1)
+                x[board[i][j]] = 1;
+                y[board[j][i]] = 1;
             }
         }
-
         for (int i = 0; i < 9; i += 3)
         {
             for (int j = 0; j < 9; j += 3)
             {
-                hashtable<char, int> chunk;
+                unordered_map<char, int> chunk;
                 for (int x = i; x < i + 3; x++)
                 {
                     bool isBreak = false;
                     for (int y = j; y < j + 3; y++)
                     {
-                        if (chunk.has(board[x][y]))
+                        if (chunk.find(board[x][y]) != chunk.end() && board[x][y] != '.')
                         {
                             return false;
                         }
-                        chunk.push(board[x][y]);
+                        chunk[board[x][y]] = 1;
                     }
                 }
             }
