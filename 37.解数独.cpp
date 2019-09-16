9f24c442a07071509cf0e1d85429f76ab40a2c79
halu886 <halu886@qq.com>
2019-09-16 10:02:21 +0800

    解数独

---
+++ 37.解数独.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=37 lang=cpp
+ *
+ * [37] 解数独
+ */
+class Solution
+{
+public:
+    void solveSudoku(vector<vector<char>> &board)
+    {
+        unordered_map[9] xs;
+        unordered_map[9] ys;
+        unordered_map[9] blocks;
+        bool isOver = false;
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if ()
+                {
+                }
+                xs[i][board[i][j]] = 1;
+                ys[y][board[i][j]] = 1;
+            }
+        }
+
+        while
+        {
+        }
+        do
+            (isOver)
+    }
+};
