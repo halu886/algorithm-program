68f4136f4ae7d791f3f76da7644f193bf3129e7f
halu886 <halu886@qq.com>
2019-10-14 09:48:17 +0800

    38

---
+++ 38.报数.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=38 lang=cpp
+ *
+ * [38] 报数
+ */
+
+// @lc code=start
+class Solution
+{
+private:
+    int count;
+
+    // todo
+    int count(n)
+    {
+        int num = n - n / 10;
+        if ()
+    }
+
+public:
+    string countAndSay(int n)
+    {
+        const count = n;
+
+        return count(n);
+    }
+};
+// @lc code=end
