bd9b848da92303ab74fc9618bce755488316d40e
halu886 <halu886@qq.com>
2019-10-22 09:34:21 +0800

    38

--- 38.报数.cpp
+++ 38.报数.cpp
@@ -7,43 +7,36 @@
 // @lc code=start
 class Solution
 {
-private:
-    int count;
-
-    // todo
-    int count(n)
-    {
-        if (!count)
-        {
-            return n
-        }
-        --count;
-        int res = 0;
-        int oldNum = n % 10;
-        n = n / 10;
-        int numCount = 0;
-        int sumCount = 0;
-        while (n)
-        {
-            int num = n % 10;
-            if (num == oldNum)
-            {
-                ++numCount;
-                continue;
-            }
-            else
-            {
-                res = numCount * (sumCount + 1) +
-            }
-        }
-    }
-
 public:
     string countAndSay(int n)
     {
-        this.count = n;
-
-        return count(1);
+        --n;
+        int res = 1;
+        while (n)
+        {
+            int tempRes = 0;
+            int numCount = 0;
+            int oldNum = 0;
+            while (res)
+            {
+                int num = res % 10;
+                res /= 10;
+                if (num == oldNum)
+                {
+                    ++numCount;
+                    continue;
+                }
+                else
+                {
+                    tempRes += numCount * pow(10, (numCount + 1)) + oldNum * pow(10, numCount);
+                    oldNum = num;
+                    numCount = 1;
+                }
+            }
+            res = tempRes;
+            --n;
+        }
+        return to_string(res);
     }
 };
 // @lc code=end
