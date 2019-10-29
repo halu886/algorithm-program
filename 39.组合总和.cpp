7acef6e559bad0d210b17c9a16ef374261e2745e
halu886 <halu886@qq.com>
2019-10-29 09:56:27 +0800

    39

--- 39.组合总和.cpp
+++ 39.组合总和.cpp
@@ -3,13 +3,41 @@
  *
  * [39] 组合总和
  */
+#include <iostream>
+#include <vector>
 
-// @lc code=start
+using namespace std;
+
 class Solution
 {
+private:
+    vector<int> candidates;
+    vector<vector<int>> res;
+    vector<int> path;
+
 public:
+    void DFS(int start, int target)
+    {
+        if (target == 0)
+        {
+            res.push_back(path);
+            return;
+        }
+        for (int i = start;
+             i < candidates.size() && target - candidates[i] >= 0; i++)
+        {
+            path.push_back(candidates[i]);
+            DFS(i, target - candidates[i]);
+            path.pop_back();
+        }
+    }
+
     vector<vector<int>> combinationSum(vector<int> &candidates, int target)
     {
+        std::sort(candidates.begin(), candidates.end());
+        this->candidates = candidates;
+        DFS(0, target);
+
+        return res;
     }
 };
-// @lc code=end
