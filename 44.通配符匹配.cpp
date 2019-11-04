c7a4557898db2a6cb50408d3ba0859f3b403d3b7
halu886 <halu886@qq.com>
2019-11-04 22:17:27 +0800

    4

--- 44.通配符匹配.cpp
+++ 44.通配符匹配.cpp
@@ -8,31 +8,32 @@
 class Solution
 {
 public:
-    bool
-    isMatch(string s, string p)
+    bool isMatch(string s, string p)
     {
-        if ((!s.size() && !p.size()) || p == "*")
+        int j = 0;
+        for (int star = 0, i = 0, last = 0; i < s.length();)
         {
-            return true;
+            if (j < p.size() && (s[i] == p[j] || p[j] == '?'))
+            {
+                ++i;
+                ++j;
+            }
+            else if (j < p.size() && p[j] == '*')
+            {
+                last = i;
+                star = ++j;
+            }
+            else if (star != 0)
+            {
+                i = ++last;
+                j = star;
+            }
+            else
+                return 0;
         }
-
-        if (!s.size() || !p.size())
-        {
-            return false;
-        }
-
-        if (p[0] == '*' && (isMatch(s.substr(0), p.substr(1)) ||
-                            isMatch(s.substr(1), p.substr(0)) ||
-                            isMatch(s.substr(1), p.substr(1))))
-        {
-            return true;
-        }
-
-        if ((s[0] == p[0] || p[0] == '?') && isMatch(s.substr(1), p.substr(1)))
-        {
-            return true;
-        }
-        return false;
+        for (; j < p.size() && p[j] == '*'; ++j)
+            ;
+        return j == p.size();
     }
 };
 // @lc code=end
