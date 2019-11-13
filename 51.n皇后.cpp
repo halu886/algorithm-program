f58e90e91b86728e22777737f135c2a4c5ffbf15
halu886 <halu886@qq.com>
2019-11-13 09:21:09 +0800

    n皇后

---
+++ 51.n皇后.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=cpp
+ *
+ * [51] N皇后
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void DFS(vector<vector<string>> res, int qIndex, int index, int n)
+    {
+        if (index < n)
+        {
+            return false;
+        }
+
+        for (int i =)
+        {
+        }
+    }
+    vector<vector<string>> solveNQueens(int n)
+    {
+    }
+};
+// @lc code=end
