d641665d17207c270dd3d1f6330a9f8d71413aa5
halu886 <halu886@qq.com>
2019-11-18 09:56:45 +0800

    51

--- 51.n皇后.cpp
+++ 51.n皇后.cpp
@@ -8,19 +8,53 @@
 class Solution
 {
 public:
-    void DFS(vector<vector<string>> res, int qIndex, int index, int n)
+    bool valid(int n, vector<int> &cols)
     {
-        if (index < n)
+        if (cols.size() <= 1)
+            return true;
+        int row = cols.size() - 1;
+        int col = cols.back();
+        for (int r = 0; r < row; ++r)
         {
-            return false;
+            int c = cols[r];
+            if (c == col || abs(c - col) == abs(r - row))
+                return false;
         }
-
-        for (int i =)
+        return true;
+    }
+    void backtrace(int n, vector<int> &cols, vector<vector<int>> &res)
+    {
+        if (!valid(n, cols))
+            return;
+        if (cols.size() == n)
         {
+            res.push_back(cols);
+            return;
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            cols.push_back(i);
+            backtrace(n, cols, res);
+            cols.pop_back();
         }
     }
     vector<vector<string>> solveNQueens(int n)
     {
+        vector<int> cols;
+        vector<vector<int>> all;
+        backtrace(n, cols, all);
+        vector<vector<string>> res;
+        for (auto &v : all)
+        {
+            vector<string> s;
+            for (auto &c : v)
+            {
+                string t(n, '.');
+                t[c] = 'Q';
+                s.push_back(t);
+            }
+            res.push_back(s);
+        }
+        return res;
     }
 };
-// @lc code=end
