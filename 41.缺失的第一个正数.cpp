a028cba260ea168f5abe8ec5fe6bdc71e89fdffa
halu886 <halu886@qq.com>
2019-11-21 10:02:52 +0800

    41

---
+++ 41.缺失的第一个正数.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=41 lang=cpp
+ *
+ * [41] 缺失的第一个正数
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        t
+    }
+};
+// @lc code=end
