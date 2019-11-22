15878851f6ce7acb6a738e6d607fc037511c7013
halu886 <halu886@qq.com>
2019-11-22 10:04:09 +0800

    41

--- 41.缺失的第一个正数.cpp
+++ 41.缺失的第一个正数.cpp
@@ -5,12 +5,25 @@
  */
 
 // @lc code=start
-class Solution
+int firstMissingPositive(vector<int> &nums)
 {
-public:
-    int firstMissingPositive(vector<int> &nums)
+    for (size_t i = 0; i < nums.size(); i++)
     {
-        t
+        if (nums[i] >= 1 && nums[i] < nums.size())
+        {
+            int t = nums[i] - 1;
+            if (nums[i] != nums[t])
+            {
+                swap(nums[i], nums[t]);
+                i -= (t > i);
+            }
+        }
     }
-};
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != i + 1)
+            return i + 1;
+    }
+    return nums.size() + 1;
+}
 // @lc code=end
