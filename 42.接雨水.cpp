35b7f5c82e593e37cbc4c183b94a626dc7c1e875
halu886 <halu886@qq.com>
2019-12-05 09:33:08 +0800

    45.跳跃游戏'

--- 42.接雨水.cpp
+++ 42.接雨水.cpp
@@ -5,22 +5,25 @@
  */
 
 // @lc code=start
-int trap(vector<int>& height)
-{
-    int left = 0, right = height.size() - 1;
-    int ans = 0;
-    int left_max = 0, right_max = 0;
-    while (left < right) {
-        if (height[left] < height[right]) {
-            height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
-            ++left;
+class Solution {
+public:
+    int trap(vector<int>& height)
+    {
+        int left = 0, right = height.size() - 1;
+        int ans = 0;
+        int left_max = 0, right_max = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
+                ++left;
+            }
+            else {
+                height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
+                --right;
+            }
         }
-        else {
-            height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
-            --right;
-        }
+        return ans;
     }
-    return ans;
-}
+};
 // @lc code=end
 
---
+++ 45.跳跃游戏-ii.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=cpp
+ *
+ * [45] 跳跃游戏 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+    	int ans = 0;
+        int end = 0;
+        int maxPos = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            maxPos = max(nums[i] + i, maxPos);
+            if (i == end)
+            {
+                end = maxPos;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
