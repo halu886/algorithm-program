a2f0bc8518402732ed5a1d9f60965f6589f3eb1d
halu886 <halu886@qq.com>
2020-04-20 10:01:52 +0800

    169

---
+++ 169.多数元素.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=cpp
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int candidate = -1;
+        int count = 0;
+        for (int num : nums)
+        {
+            if (num == candidate)
+                ++count;
+            else if (--count < 0)
+            {
+                candidate = num;
+                count = 1;
+            }
+        }
+        return candidate;
+    }
+};
+// @lc code=end
